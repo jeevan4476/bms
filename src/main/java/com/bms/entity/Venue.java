888944fe0d01e8a4db7a0caaf92c8595c8c24796
karash10 <kappalaharshith@gmail.com>
2026-03-09 23:45:16 +0530

    implemented the admin and the user related api

--- src/main/java/com/bms/entity/Venue.java
+++ src/main/java/com/bms/entity/Venue.java
@@ -1,8 +1,12 @@
 package com.bms.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Venue extends BaseEntity {
 
     private String name;
--- src/main/java/com/bms/repository/SeatRepository.java
+++ src/main/java/com/bms/repository/SeatRepository.java
@@ -1,8 +1,13 @@
 package com.bms.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bms.entity.Seat;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findByVenueId(Long venueId);
+
 }
